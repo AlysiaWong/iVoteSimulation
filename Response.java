b13811b7700a6b864f31b153263b2a5de6dd6c54
Alysia Wong <39535350+AlysiaWong@users.noreply.github.com>
2020-09-29 14:06:05 -0700

    Update Response.java

--- Response.java
+++ Response.java
@@ -1,9 +1,26 @@
 // Alysia Wong
 // CS 3560 Assignment 1
-// Response.java - Interface
+// Response.java - prints out response choices that students can choose from
 
 package iVoteSimulation;
 
-public interface Response {
-	public void printResponseChoices();
+public class Response {
+	private String responseA;
+	private String responseB;
+	private String responseC;
+	private String responseD;
+	
+	public Response(String a, String b, String c, String d) {
+		this.responseA = a;
+		this.responseB = b;
+		this.responseC = c;
+		this.responseD = d;
+	}
+	
+	public void printResponseChoices() {
+		System.out.println("A) " + responseA
+						  +" B) " + responseB
+						  +" C) " + responseC
+						  +" D) " + responseD);
+	}
 }
